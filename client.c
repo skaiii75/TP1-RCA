8cc02baa7411aade9212c702cd705b9757c5d525
Jonas <maouchejonas@gmail.com>
2026-05-21 15:14:04 +0200

    Etape 1 - Installer le client

--- client.c
+++ client.c
@@ -1,80 +1,54 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
-#include <signal.h>
-#include <arpa/inet.h>
-#include <netinet/in.h>
-#include <sys/socket.h>
 
-#define MSG_LEN 100         // taille max des messages autorisés
-#define IN_FILT "%[^\n]%*c" // filtre le prompt : récupère tout jusqu'au premier retour à la ligne
-#define CMD_QUIT "/q"       // commande du prompt pour quitter
-#define SERVER_IP "169.254.61.46"
-#define SERVER_PORT 4242
+#define STR_HELPER(x) #x
+#define STR(x) STR_HELPER(x)
 
-int main(int argc , char *argv[]) {
+#define MSG_LEN 100
+#define MSG_SCAN_LEN 99
+#define IN_FILT "%" STR(MSG_SCAN_LEN) "[^\n]%*c"
+#define CMD_QUIT "/q"
+
+int main(int argc, char *argv[]) {
 	(void)argc;
 	(void)argv;
 
-	/* Ce buffer recevra les messages à transmettre */
+	/*
+	 * Étape 1 - Installation
+	 *
+	 * Compilation :
+	 *     cc -o client client.c
+	 *
+	 * Exécution :
+	 *     ./client
+	 *
+	 * Pour quitter le programme, saisir la commande /q.
+	 */
 	char buffer[MSG_LEN];
-
-	/* Connexion au serveur du chat général */
-	int sock = socket(AF_INET, SOCK_STREAM, 0);
-	if(sock == -1) {
-		perror("Erreur création socket");
-		return EXIT_FAILURE;
-	}
-
-	struct sockaddr_in server;
-	memset(&server, 0, sizeof(server));
-	server.sin_family = AF_INET;
-	server.sin_port = htons(SERVER_PORT);
-
-	if(inet_pton(AF_INET, SERVER_IP, &server.sin_addr) != 1) {
-		perror("Adresse IP serveur invalide");
-		close(sock);
-		return EXIT_FAILURE;
-	}
-
-	if(connect(sock, (struct sockaddr *)&server, sizeof(server)) == -1) {
-		perror("Erreur connexion serveur");
-		close(sock);
-		return EXIT_FAILURE;
-	}
-
-	/* Fork: création d'un thread enfant */
-	int pid = fork();
-	if(pid == -1) {
-		perror("Erreur fork");
-		close(sock);
-		return EXIT_FAILURE;
-	}
-
-	/* Flag pour arrêter le programme */
 	int quit = 0;
+
 	while(!quit) {
-		if(pid==0) { // thread enfant
-			/* L'envoi des messages sera ajouté à l'étape 4 */
-			sleep(1);
-		} else { // thread parent
-			/* Réception des messages transmis par le serveur */
-			int nb_read = recv(sock, buffer, MSG_LEN - 1, 0);
-			if(nb_read <= 0) {
-				printf("Déconnecté du serveur\n");
+		/*
+		 * IN_FILT lit le contenu saisi dans le terminal jusqu'au premier
+		 * retour à la ligne. MSG_SCAN_LEN évite de dépasser la taille du
+		 * buffer et garde une place pour le caractère final '\0'.
+		 */
+		int nb_scan = scanf(IN_FILT, buffer);
+
+		if(nb_scan == 1) {
+			if(strcmp(buffer, CMD_QUIT) == 0) {
 				quit = 1;
 			} else {
-				buffer[nb_read] = '\0';
-				printf("%s\n", buffer);
+				printf("Message saisi : %s\n", buffer);
 			}
+		} else if(nb_scan == EOF) {
+			quit = 1;
+		} else {
+			/* Cas d'une ligne vide : on consomme le '\n' resté en entrée. */
+			getchar();
 		}
 	}
-	if(pid != 0) { // thread parent
-		/* Termine le thread enfant */
-		kill(pid, SIGTERM);
-	}
-	close(sock);
 
-	return 0;
+	return EXIT_SUCCESS;
 }
